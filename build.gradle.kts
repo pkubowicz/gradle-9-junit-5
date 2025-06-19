4ff70124736e577a58106e466222bed6518ac69d
Piotr Kubowicz <piotr.kubowicz@gmail.com>
2025-06-19 19:45:36 +0200

    Add code and GitHub action

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    java
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.springframework.boot:spring-boot-dependencies:3.4.6"))
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
